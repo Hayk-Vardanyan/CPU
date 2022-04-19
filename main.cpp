9091a37fc76d0115fa62dfc4cbd05ff229cf0978
HaykVardanyan2004 <90803123+HaykVardanyan2004@users.noreply.github.com>
2022-04-19 15:13:21 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,14 @@
 int main()
 {
     Ram ram;
-    ram.FillMemory("cpu.txt");
-    Cpu cpu;
-    cpu.ReadFromRam(ram);
+    std::string path;
+    std::cout << "Enter the name of file to read from\n";
+    std::cin >> path;
+    if(ram.fillMemory(path)) {
+    	Cpu cpu;
+    	if(!cpu.ReadFromRam(ram)) std::cout << "unkown command\n";
+    }
+    else {
+		std::cout << "failed to fill memory\n";
+	}	
 }
